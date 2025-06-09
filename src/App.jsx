64b651cd6956ba0c119000452ed4e94932fe92ee
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2025-06-09 22:07:28 +0530

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Compnents/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
